bf5112d4297741c02a9fc2c17d2dfd5ef9ba5f1e
Rawan Aladdin <raladdin@my.bcit.ca>
2024-04-07 22:21:14 -0700

    base upload of project

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+import "@/styles/bootstrap.min.css";
+import { useEffect } from "react";
+
+
+export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return <Component {...pageProps} />;
+}
+
