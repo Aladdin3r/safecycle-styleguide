3f62f52533249323c860ab8b341cdc1f6145e6ed
Andrew <andrewturitsa@gmail.com>
2024-04-09 23:06:35 -0700

    swapping images 3

--- .history/pages/index_20240409230548.js
+++ .history/pages/index_20240409230548.js
@@ -186,7 +186,7 @@ export default function Home() {
         <img className={styles.traffic} src="/traffic.png" alt="Traffic"></img>
         <div className={styles.graphicDesc}>Many cars, tightly packed together, but with no outright or hazard or danger.</div>
         <div className={styles.graphicName}>Road Closed:</div>
-        <img className={styles.roadClosed} src="/closed.png" alt="Road Closed"></img>
+        <img className={styles.roadClosed} src="/roadClosed.png" alt="Road Closed"></img>
         <div className={styles.graphicDesc}>An iconic piece of equipment that is easily recognized as “Do not drive past this sign”.</div>
         <div className={styles.graphicExpl}>Our app will aim to make use numerous descriptive vector graphics accompanied by text that can remain simple while clearly depicting a specific concept. The aim of these graphics is to allow users of all experience in app usage to quickly understand “what does what” in our app. SafeCycle will often need to provide a lot of information without a large amount of pages so we will attempt to make use of the “show don’t tell” method as often as possible for efficiencies’ sake.</div>
         <div className={styles.graphicTitle}>IMAGERY</div>
