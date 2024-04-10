1e2016fef46ca774e1b88cf0bec934bcbb9789b6
Andrew <andrewturitsa@gmail.com>
2024-04-09 22:53:24 -0700

    margin fixes 2

--- .history/pages/index_20240409225222.js
+++ .history/pages/index_20240409225222.js
@@ -180,13 +180,13 @@ export default function Home() {
         <div className={styles.graphicTitle}>GRAPHICS</div>
         <div className={styles.graphicName}>Here are some examples of icons we will be using:</div>
         <div className={styles.graphicName}>Car accident:</div>
-        <img className={styles.carCrash} src="/carCrash.png" height={100} width={130} alt="Car Accident"></img>
+        <img className={styles.carCrash} src="/carCrash.png" height={20} width={20} alt="Car Accident"></img>
         <div className={styles.graphicDesc}>Red, jarring and spiked graphic implies danger and an impact of some sort.</div>
         <div className={styles.graphicName}>Traffic Jam:</div>
-        <img className={styles.traffic} src="/traffic.png" height={100} width={130} alt="Traffic"></img>
+        <img className={styles.traffic} src="/traffic.png" alt="Traffic"></img>
         <div className={styles.graphicDesc}>Many cars, tightly packed together, but with no outright or hazard or danger.</div>
         <div className={styles.graphicName}>Road Closed:</div>
-        <img className={styles.RoadClosed} src="/RoadClosed.png" height={100} width={130} alt="Raod Closed"></img>
+        <img className={styles.RoadClosed} src="/RoadClosed.png" alt="Raod Closed"></img>
         <div className={styles.graphicDesc}>An iconic piece of equipment that is easily recognized as “Do not drive past this sign”.</div>
         <div className={styles.graphicExpl}>Our app will aim to make use numerous descriptive vector graphics accompanied by text that can remain simple while clearly depicting a specific concept. The aim of these graphics is to allow users of all experience in app usage to quickly understand “what does what” in our app. SafeCycle will often need to provide a lot of information without a large amount of pages so we will attempt to make use of the “show don’t tell” method as often as possible for efficiencies’ sake.</div>
         <div className={styles.graphicTitle}>IMAGERY</div>
